3d9a388e515dbeb3c36958f25fd7979d71f6e886
Asa Holland <hollandasa@gmail.com>
2021-03-27 02:37:44 -0700

    setting up mongodb

--- website/__init__.py
+++ website/__init__.py
@@ -1,53 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-# import os to manage the file connections
-import os
-import json
-import datetime
-
-
-# import the general python mongodb driver
-import pymongo
-
-# import the specific module to handshake flask with mongoDB
+from flask import Flask
 from flask_pymongo import PyMongo
 
-from bson import ObjectId
-
-# Connects to fl
-class JSONEncoder(json.JSONEncoder):
-    ''' extend json-encoder class'''
-
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-            if isinstance(o, datetime.datetime):
-                return str(o)
-                return json.JSONEncoder.default(self, o)
+from website.database import mongo
 
 
-# 
+import os
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'fdjkhngkjefj dk'
 
-    # Configure the connection to the database
-    client = pymongo.MongoClient("mongodb+srv://HackOR_2021_user:cmh2LPJzNgOYKX8y@hackor2021cluster.d6r9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    app.config['MONGO_URI'] = "mongodb+srv://HackOR_2021_user:cmh2LPJzNgOYKX8y@hackor2021cluster.d6r9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    app.config['MONGO_DBNAME'] = os.environ.get('minerva',app.name)
+
+    mongo = PyMongo()
+
+    mongo.init_app(app)
     
-    # isolate the desired database from MongoDb
-    db = client.minerva
-
-    # isolate each of the collections used by the database
-    artists=db.artists
-    fans=db.fans
-    tracks=db.tracks
-    shows=db.shows
-    albums=db.albums
-
-    # Creates a wrapper around the app instance that is tied to the database
-
     from .views import views
 
     app.register_blueprint(views, url_prefix='/')
--- website/views.py
+++ website/views.py
@@ -1,10 +1,24 @@
 from flask import Blueprint, render_template
 
+
+from pymongo import ReturnDocument, MongoClient
+
+from website import mongo
+
+
+def get_artists():
+    client = MongoClient("mongodb+srv://HackOR_2021_user:cmh2LPJzNgOYKX8y@hackor2021cluster.d6r9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = client.minerva
+    artists = list(db.artists.find())
+    return artists
+
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("index.html")
+    db_artists = get_artists()
+    print(db_artists[0]['first_name'])
+    return render_template("index.html", artists = db_artists)
 
 @views.route('/artist-home')
 def artist_home():
