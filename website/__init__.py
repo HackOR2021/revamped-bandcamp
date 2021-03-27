9b2b06fdf99ea00ac1292e49f8722f402940acc2
Asa Holland <hollandasa@gmail.com>
2021-03-27 01:02:27 -0700

    Update __init__.py
    adding initial connection to mongodb

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
+# import os to manage the file connections
+import os
+import json
+import datetime
+
+
+# import to connect to MongoDb database
+from flask_pymongo import PyMongo
+
+from bson import ObjectId
+
+# Connects to fl
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+# 
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'fdjkhngkjefj dk'
+
+    # Configure the connection to the database
+    client = pymongo.MongoClient("mongodb+srv://admin:zdEGl1wIgyJ47LVZ@hackplanner-ahnzl.gcp.mongodb.net/test?retryWrites=true&w=majority")
+    client = pymongo.MongoClient("mongodb+srv://HackOR_2021_user:cmh2LPJzNgOYKX8y@hackor2021cluster.d6r9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+	
+	# isolate the desired database from MongoDb
+	db = client.minerva
+
+	# isolate each of the collections used by the database
+	artists=db.artists
+	fans=db.fans
+	tracks=db.tracks
+	shows=db.shows
+	albums=db.albums
+
+    # Creates a wrapper around the app instance that is tied to the database
+    app = PyMongo(app)
 
     from .views import views
 
