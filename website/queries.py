0fe0c02f19a9756cc0628d9d7320f3acc246020e
Asa Holland <hollandasa@gmail.com>
2021-03-27 03:07:14 -0700

    Update queries.py

--- website/queries.py
+++ website/queries.py
@@ -1,7 +1,22 @@
 from pymongo import MongoClient
 
+
+
+def connect_minerva_db():
+	"""
+	Create a connection to the live mongoDB server, select the minerva database and return it so we can query
+	"""
+	client = MongoClient("mongodb+srv://HackOR_2021_user:cmh2LPJzNgOYKX8y@hackor2021cluster.d6r9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    return client.minerva
+
+
 def get_artists():
-    client = MongoClient("mongodb+srv://HackOR_2021_user:cmh2LPJzNgOYKX8y@hackor2021cluster.d6r9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    db = client.minerva
+	"""
+	Returns all artists on the minerva DB
+	"""
+	# Connect to database
+	db = connect_minerva_db()
+
+	# return all the artists as a list
     artists = list(db.artists.find())
     return artists
