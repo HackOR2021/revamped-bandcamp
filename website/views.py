4af658aa9563e5e7192d72da5068c42b50eca01f
Velavee <59717974+Velavee@users.noreply.github.com>
2021-03-26 22:17:10 -0700

    Finished add tracks html started edit tracks

--- website/views.py
+++ website/views.py
@@ -9,3 +9,11 @@ def home():
 @views.route('/artist-home')
 def artist_home():
     return render_template("artist_home.html")
+
+@views.route('/add-tracks')
+def add_tracks():
+    return render_template("new_track.html")
+
+@views.route('/edit-tracks')
+def edit_tracks():
+    return render_template("edit_track.html")
